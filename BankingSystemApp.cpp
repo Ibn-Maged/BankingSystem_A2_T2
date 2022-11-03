6e779849bda135f3b6a322b1192c743c461c32aa
Youssef Moataz <yousefcrunshy03@gmail.com>
2022-11-03 21:00:56 +0200

    Added header with project description.

--- BankingSystemApp.cpp
+++ BankingSystemApp.cpp
@@ -1,3 +1,17 @@
+/*
+FCAI - Programming 2 - 2022 - Assignment 2 - Task 2
+Program Name:           Banking System
+Program Description:    This program is a bank system which -Creates clients and two types of bank accounts
+                        -Stores data per run -Withdraw money -Deposit money
+Last Modification Date: November 2, 2022
+Version:                v1.4.0
+Authors:                Youssef Moataz, Hassan Magdy,   Mohamed Maged
+ID:                     20210488,       20210126,       20210352
+Group:                  A
+Labs:                   S9-S10
+Purpose:                Master OOP by making such great banking system project
+*/
+
 #include "BankingSystemFunctions.cpp"
 
 int main() {
